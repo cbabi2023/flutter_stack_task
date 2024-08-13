2e3dc5f7438cec87cbeb76256a7b01f9c7606ded
cbabi2023 <abijithcbbsc2021@gmail.com>
2024-08-13 15:26:55 +0530

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,56 @@ class MyApp extends StatelessWidget {
                           borderRadius: BorderRadius.circular(45),
                         ),
                       ),
+                      const SizedBox(height: 30),
+                      const Text(
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.psum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                        textAlign: TextAlign.justify,
+                      )
                     ],
+                  ),
+                ),
+                Positioned(
+                  top: 56,
+                  left: 40,
+                  child: Container(
+                    height: 200,
+                    width: 260,
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 105,
+                  left: 107,
+                  child: Container(
+                    height: 100,
+                    width: 120,
+                    decoration: BoxDecoration(
+                      color: Colors.green[800],
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 215,
+                  left: 257,
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    decoration: const BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(60),
+                        bottomRight: Radius.circular(60),
+                      ),
+                    ),
                   ),
                 ),
               ],
